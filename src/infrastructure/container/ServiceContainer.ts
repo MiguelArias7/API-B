3e11ab2f37f6d8b50e73028aa73781755036f5e5
Arias7 <miguelariasgiraldo2000@gmail.com>
2025-07-07 00:20:00 -0500

    feat: add pokemon and digimon services on container

--- src/infrastructure/container/ServiceContainer.ts
+++ src/infrastructure/container/ServiceContainer.ts
@@ -1,17 +1,28 @@
+import { DigimonPort } from "../../adapters/external/DigimonAdapter";
+import { PokemonPort } from "../../adapters/external/PokemonAdapter";
+import { DataFindById } from "../../application/useCases/DataFindById";
 import { LogCreate } from "../../application/useCases/LogCreate";
 import { LogFindAll } from "../../application/useCases/LogFindAll";
 import { LogFindById } from "../../application/useCases/LogFindById";
-import { InMemoryLogRepository } from "../databases/InMemoryLogRepository";
+import { Digimon } from "../../domain/models/Digimon";
+import { Pokemon } from "../../domain/models/Pokemon";
 import { PostgresLogRepository } from "../databases/PostgresLogRepository";
 
 
 // const logRepository = new InMemoryLogRepository();
 const logRepository = PostgresLogRepository.instance;
 
+const PokemonFranchisePort: PokemonPort = new PokemonPort();
+const DigimonFranchisePort: DigimonPort = new DigimonPort();
+
 export const ServiceContainer = {
     log: {
         create: new LogCreate(logRepository),
         getAll: new LogFindAll(logRepository),
         getById: new LogFindById(logRepository),
-    }
+    },
+    data: {
+        getDigimonById: new DataFindById<Digimon>(DigimonFranchisePort),
+        getPokemonByPokemon: new DataFindById<Pokemon>(PokemonFranchisePort),
+    },
 }
