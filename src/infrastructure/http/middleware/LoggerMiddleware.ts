3e6465b640f31b3650c3a0672d473b68a874bee7
Arias7 <miguelariasgiraldo2000@gmail.com>
2025-07-07 00:22:46 -0500

    feat: add loggerMiddleware

---
+++ src/infrastructure/http/middleware/LoggerMiddleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { ServiceContainer } from "../../container/ServiceContainer";
+import e = require("express");
+
+export function loggerMiddleware() {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const { metadata: metadataJson } = req.query;
+        const { franchise } = req.params;
+        const metadata = typeof metadataJson === "string" ? JSON.parse(metadataJson) : metadataJson;
+
+        try {
+
+            await ServiceContainer.log.create.execute(franchise, "v1", metadata);
+        } catch (error) {
+            // Log the error with relevant details
+            const { franchise } = req.params;
+            const errorMessage = error instanceof Error ? error.message : String(error);
+            await ServiceContainer.log.create.execute(franchise, "v1", metadata, errorMessage);
+        }
+        next();
+    };
+}
